c958ee06c5d5b1914e17656bbb581ef1250e627c
Probyte5D <primula.robustelli@gmail.com>
2025-04-24 03:19:23 +0200

    COMMIT

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Sezione per la gestione dello sfondo della prima sezione
+// Sezione per la gestione del cambio dello sfondo nella prima sezione
 const backgrounds = [
   "url('./images/restaurant-background-2.png')",
   "url('./images/immagine2.png')",
@@ -9,7 +9,6 @@ const backgrounds = [
 let backgroundIndex = 0;
 const section = document.querySelector('.section-1');
 
-// Cambia l'immagine di sfondo ogni 4 secondi
 setInterval(() => {
   section.style.backgroundImage = `
     linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.575)),
@@ -19,43 +18,36 @@ setInterval(() => {
   backgroundIndex = (backgroundIndex + 1) % backgrounds.length;
 }, 4000);
 
+// Sezione per la gestione del cambio dei testi visibili nella prima sezione
+const texts = document.querySelectorAll('.section-1-Text .text-item');
+let i = 0;
 
-// Sezione per la gestione del cambio dei testi nella prima sezione
-const texts = document.querySelectorAll('.section-1-Text .text-item'); // Seleziona tutti i testi
-let i = 0; // Variabile per tracciare l'indice della parola corrente
-
-// Cambia il testo visibile ogni 4 secondi
 setInterval(() => {
-  const currentText = document.querySelector('.section-1-Text .text-item.change'); // Seleziona il testo con la classe 'change'
+  const currentText = document.querySelector('.section-1-Text .text-item.change');
 
   if (currentText) {
-    currentText.classList.remove('change'); // Rimuove la classe 'change' dal testo corrente
+    currentText.classList.remove('change');
 
-    // Se siamo oltre l'ultimo testo, ripristina al primo
     if (i >= texts.length) {
-      texts[0].classList.add('change'); // Aggiungi la classe 'change' al primo testo
-      i = 0; // Resetta l'indice a 0
+      texts[0].classList.add('change');
+      i = 0;
     } else {
-      texts[i].classList.add('change'); // Aggiungi la classe 'change' al testo successivo
-      i++; // Incrementa l'indice
+      texts[i].classList.add('change');
+      i++;
     }
   }
-}, 4000); // Cambia il testo ogni 4 secondi
+}, 4000);
 
-
-// Sezione per nascondere e mostrare la navbar durante lo scroll
+// Sezione per nascondere o mostrare la navbar durante lo scroll
 let lastScroll = 0;
 const navbar = document.querySelector('.navbar');
 
-// Rileva il movimento dello scroll per nascondere o mostrare la navbar
 window.addEventListener('scroll', () => {
   const currentScroll = window.scrollY;
 
   if (currentScroll > lastScroll) {
-    // Scroll verso il basso → nascondi la navbar
     navbar.style.top = "-100px";
   } else {
-    // Scroll verso l'alto → mostra la navbar
     navbar.style.top = "0";
   }
 
